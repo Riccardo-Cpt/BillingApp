ac7b309443a9920d9e2b483677a8cae240a74599
unknown <riccardo.9giuliani.6@gmail.com>
2025-08-07 09:09:20 +0200

    First ApI created, but unable to access server where I can interact with

--- python-api/app/billing_track/billing_app/urls.py
+++ python-api/app/billing_track/billing_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import anagraphic_list
 
 urlpatterns = [
-    path('api/anagraphic/', anagraphic_list.as_view(), name='anagraphic-list'),
+    path('billing_app/anagraphic/', anagraphic_list, name='anagraphic-list'),
 ]
